54dee956f75f98e5a96a0a16a76d6a23f0e1e117
Vikkar <viktorka2014@gmail.com>
2022-05-09 16:03:09 +0200

    Update youtube.js

--- videolife/storage/youtube.js
+++ videolife/storage/youtube.js
@@ -6,43 +6,43 @@ function resetYTVids() {
          "title": "Canadian Reacts to CANADIAN History Matters Videos!",
          "date": "2022-05-07T16:30:00Z",
          "thumbnail": "https://i.ytimg.com/vi/amSfvVgDkjc/maxresdefault.jpg",
-         "views": "152"
+         "views": "189"
         },
         {
          "title": "A Complete History of World War 1 in Countryballs - Reaction",
          "date": "2022-05-03T18:30:46Z",
          "thumbnail": "https://i.ytimg.com/vi/9xNZ46YUGnA/maxresdefault.jpg",
-         "views": "132"
+         "views": "137"
         },
         {
          "title": "The Borders of the 22nd Century? - Whatifalthist Reaction",
          "date": "2022-04-30T18:30:20Z",
          "thumbnail": "https://i.ytimg.com/vi/W_-8iB4g8sk/maxresdefault.jpg",
-         "views": "317"
+         "views": "347"
         },
         {
          "title": "Canadian Reacts AGAIN to WW2 PART 2 - Oversimplified",
          "date": "2022-04-29T17:30:11Z",
          "thumbnail": "https://i.ytimg.com/vi/uqIMzxkFH0c/maxresdefault.jpg",
-         "views": "1702"
+         "views": "1785"
         },
         {
          "title": "Canadian Reacts to WW2 - Oversimplified",
          "date": "2022-04-27T17:00:30Z",
          "thumbnail": "https://i.ytimg.com/vi/pA_z5sFwnVQ/maxresdefault.jpg",
-         "views": "1295"
+         "views": "1321"
         },
         {
          "title": "Canadian Reacts to How Canada Will Fall by Whatifalthist",
          "date": "2022-04-24T18:45:00Z",
          "thumbnail": "https://i.ytimg.com/vi/j4plTWf4HMM/maxresdefault.jpg",
-         "views": "568"
+         "views": "605"
         },
         {
          "title": "When the Historical Horst Game Lasts Past 1944...",
          "date": "2022-03-23T18:00:17Z",
          "thumbnail": "https://i.ytimg.com/vi/ptfRtJ9DqEQ/maxresdefault.jpg",
-         "views": "200"
+         "views": "204"
         },
         {
          "title": "FUNNIEST RP Game Ever? ROLEPLAY as Sweden!",
@@ -60,19 +60,19 @@ function resetYTVids() {
          "title": "LOSING AS AC HUNGARY ft. SeeMops",
          "date": "2022-01-03T22:15:31Z",
          "thumbnail": "https://i.ytimg.com/vi/hizoEavk8wo/hqdefault.jpg",
-         "views": "153"
+         "views": "154"
         },
         {
          "title": "SO I 1v1'd MY STREAM",
          "date": "2021-11-15T19:00:15Z",
          "thumbnail": "https://i.ytimg.com/vi/wzMf77ROUYc/maxresdefault.jpg",
-         "views": "141"
+         "views": "142"
         },
         {
          "title": "MORE WORLD CONQUESTS? - VideoLife Reacts to No Step Back Achievements",
          "date": "2021-11-11T18:00:06Z",
          "thumbnail": "https://i.ytimg.com/vi/9WwUEe61K74/hqdefault.jpg",
-         "views": "218"
+         "views": "219"
         }
     ]
 }
