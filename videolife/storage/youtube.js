11caabba97d3cdfb1497874456ed57c389b3e8dd
Vikkar <viktorka2014@gmail.com>
2022-05-14 16:29:46 +0200

    Update youtube.js

--- videolife/storage/youtube.js
+++ videolife/storage/youtube.js
@@ -3,52 +3,70 @@ let ytVids = [];
 function resetYTVids() {
     ytVids = [
         {
+         "title": "Oversimplified Hitler Reaction PART 2!",
+         "date": "2022-05-13T18:30:08Z",
+         "thumbnail": "https://i.ytimg.com/vi/DAh0tbstJRI/maxresdefault.jpg",
+         "views": "229"
+        },
+        {
+         "title": "The BANNED, INFAMOUS Oversimplified Video! Hitler (Part 1) Reaction",
+         "date": "2022-05-11T18:30:32Z",
+         "thumbnail": "https://i.ytimg.com/vi/QJe_JuTjW8Q/maxresdefault.jpg",
+         "views": "655"
+        },
+        {
+         "title": "Is WW3 REALLY About To Start? (Probably Not) - Whatifalthist Reaction Video",
+         "date": "2022-05-09T19:00:22Z",
+         "thumbnail": "https://i.ytimg.com/vi/h9bUI29WgJQ/hqdefault.jpg",
+         "views": "545"
+        },
+        {
          "title": "Canadian Reacts to CANADIAN History Matters Videos!",
          "date": "2022-05-07T16:30:00Z",
          "thumbnail": "https://i.ytimg.com/vi/amSfvVgDkjc/maxresdefault.jpg",
-         "views": "189"
+         "views": "426"
         },
         {
          "title": "A Complete History of World War 1 in Countryballs - Reaction",
          "date": "2022-05-03T18:30:46Z",
          "thumbnail": "https://i.ytimg.com/vi/9xNZ46YUGnA/maxresdefault.jpg",
-         "views": "137"
+         "views": "186"
         },
         {
          "title": "The Borders of the 22nd Century? - Whatifalthist Reaction",
          "date": "2022-04-30T18:30:20Z",
          "thumbnail": "https://i.ytimg.com/vi/W_-8iB4g8sk/maxresdefault.jpg",
-         "views": "347"
+         "views": "522"
         },
         {
          "title": "Canadian Reacts AGAIN to WW2 PART 2 - Oversimplified",
          "date": "2022-04-29T17:30:11Z",
          "thumbnail": "https://i.ytimg.com/vi/uqIMzxkFH0c/maxresdefault.jpg",
-         "views": "1785"
+         "views": "2221"
         },
         {
          "title": "Canadian Reacts to WW2 - Oversimplified",
          "date": "2022-04-27T17:00:30Z",
          "thumbnail": "https://i.ytimg.com/vi/pA_z5sFwnVQ/maxresdefault.jpg",
-         "views": "1321"
+         "views": "1590"
         },
         {
          "title": "Canadian Reacts to How Canada Will Fall by Whatifalthist",
          "date": "2022-04-24T18:45:00Z",
          "thumbnail": "https://i.ytimg.com/vi/j4plTWf4HMM/maxresdefault.jpg",
-         "views": "605"
+         "views": "780"
         },
         {
          "title": "When the Historical Horst Game Lasts Past 1944...",
          "date": "2022-03-23T18:00:17Z",
          "thumbnail": "https://i.ytimg.com/vi/ptfRtJ9DqEQ/maxresdefault.jpg",
-         "views": "204"
+         "views": "205"
         },
         {
          "title": "FUNNIEST RP Game Ever? ROLEPLAY as Sweden!",
          "date": "2022-01-23T18:00:03Z",
          "thumbnail": "https://i.ytimg.com/vi/AfbOel3jpbY/maxresdefault.jpg",
-         "views": "132"
+         "views": "138"
         },
         {
          "title": "When Russia has to save the Allies...again | WINNING as AC CANADA",
@@ -60,19 +78,19 @@ function resetYTVids() {
          "title": "LOSING AS AC HUNGARY ft. SeeMops",
          "date": "2022-01-03T22:15:31Z",
          "thumbnail": "https://i.ytimg.com/vi/hizoEavk8wo/hqdefault.jpg",
-         "views": "154"
+         "views": "155"
         },
         {
          "title": "SO I 1v1'd MY STREAM",
          "date": "2021-11-15T19:00:15Z",
          "thumbnail": "https://i.ytimg.com/vi/wzMf77ROUYc/maxresdefault.jpg",
-         "views": "142"
+         "views": "144"
         },
         {
          "title": "MORE WORLD CONQUESTS? - VideoLife Reacts to No Step Back Achievements",
          "date": "2021-11-11T18:00:06Z",
          "thumbnail": "https://i.ytimg.com/vi/9WwUEe61K74/hqdefault.jpg",
-         "views": "219"
+         "views": "222"
         }
     ]
 }
